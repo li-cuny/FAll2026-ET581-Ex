d102427e88fa2159fa919a7706420e87920cf8cb
li-cuny <changlin.li@qcc.cuny.edu>
2026-09-03 23:45:47 +0000

    Lab1 second commit

---
+++ Lab1/MathOperatorTest.java
@@ -0,0 +1,14 @@
+public class MathOperatorTest {
+    public static void main(String[] args){
+        int x; // declare var
+        x = 0; // assignment
+
+        int y = 1; // dec + assign
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+        System.out.println(" 7/2 = " + 7/2);
+        System.out.println(" 7%2 = " + 7%2);
+    }
+}
