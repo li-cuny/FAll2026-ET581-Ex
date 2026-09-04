4c2aa63a0802db2beba6f3f1f4aa4c050ca35b4c
li-cuny <changlin.li@qcc.cuny.edu>
2026-09-04 00:28:29 +0000

    inputtest

--- Lab1/MathOperatorTest.java
+++ Lab1/MathOperatorTest.java
@@ -10,5 +10,19 @@ public class MathOperatorTest {
 
         System.out.println(" 7/2 = " + 7/2);
         System.out.println(" 7%2 = " + 7%2);
+
+        int a = 1;
+        long b = 200L;
+        long c = a + b;
+        // int d = a + b;
+        System.out.println("c = " + c);
+        int e = 2;
+        double f = 3.5;
+        double g = e + f;
+        System.out.println("g = " + g);
+
+        double doubleValue = 2.5;
+        int intValue = (int) doubleValue;
+        System.out.println("int value of 2.5 " + intValue);
     }
 }
