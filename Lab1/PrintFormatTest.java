f606a6a4c4b97cc654ae58397068e9446fc3463d
li-cuny <changlin.li@qcc.cuny.edu>
2026-09-08 23:12:00 +0000

    print format test commit

---
+++ Lab1/PrintFormatTest.java
@@ -0,0 +1,8 @@
+public class PrintFormatTest {
+    public static void main(String[] args) {
+
+
+        System.out.printf(" Hello \t: %s\n", "Li"); // String value
+        // \t tab char \n new line char 
+    }
+}
